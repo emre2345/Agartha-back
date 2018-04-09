7dfc6d8836c377436af9dd93e25e577df96c4761
Jörgen Andersson <jorgen@kollektiva.se>
2018-04-09 10:36:57 +0200

    Empty commit for automatic deploy

--- modules/web/starter/src/main/java/agartha/starter/StarterMain.java
+++ modules/web/starter/src/main/java/agartha/starter/StarterMain.java
@@ -5,6 +5,7 @@ import agartha.site.ServerKt;
 /**
  * Purpose of this file is to start the Site WebServer
  * In this package because was not able to start Kotlin from Heroku
+ * 
  * <p>
  * Created by Jorgen Andersson on 2018-04-06.
  */
