462a3b838726535d90c372b9652494b765f3b191
Jörgen Andersson <jorgen@kollektiva.se>
2018-04-23 16:18:38 +0200

    Tests

--- modules/web/site/src/test/kotlin/agartha/site/objects/PractitionerTest.kt
+++ modules/web/site/src/test/kotlin/agartha/site/objects/PractitionerTest.kt
@@ -4,7 +4,6 @@ import agartha.data.objects.PractitionerDBO
 import agartha.data.objects.SessionDBO
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.time.LocalDateTime
 import java.util.*
 
 /**
@@ -21,21 +20,38 @@ class PractitionerTest {
     }
 
     @Test
-    fun practitionerLastSessionTime_WithSessions_zero() {
+    fun practitionerLastSessionTime_WithSessions_20() {
         val user = Practitioner(PractitionerDBO(
                 listOf(
-                       // SessionDBO(0, "", true, LocalDateTime.parse(""), null)
+                        SessionDBO(0, "Yoga", false, Date(2018, 4, 18, 12, 0, 0), Date(2018, 4, 18, 12, 40, 0)),
+                        SessionDBO(1, "Mindfulness", false, Date(2018, 4, 19, 12, 0, 0), Date(2018, 4, 19, 12, 30, 0)),
+                        SessionDBO(2, "Yes", false, Date(2018, 4, 20, 12, 0, 0), Date(2018, 4, 20, 12, 20, 0))
                 ),
                 Date(),
                 "abc"
 
         ))
-        assertThat(user.lastSessionTime).isEqualTo(0)
+        assertThat(user.lastSessionTime).isEqualTo(20)
     }
 
     @Test
     fun practitionerTotalSessionTime_WithNoSessions_zero() {
         val user = Practitioner(null, null, null)
         assertThat(user.totalSessionTime).isEqualTo(0)
+    }
+
+    @Test
+    fun practitionerTotalSessionTime_WithSessions_80() {
+        val user = Practitioner(PractitionerDBO(
+                listOf(
+                        SessionDBO(0, "Yoga", false, Date(2018, 4, 18, 12, 0, 0), Date(2018, 4, 18, 12, 40, 0)),
+                        SessionDBO(1, "Mindfulness", false, Date(2018, 4, 19, 12, 0, 0), Date(2018, 4, 19, 12, 30, 0)),
+                        SessionDBO(2, "Yes", false, Date(2018, 4, 20, 12, 0, 0), Date(2018, 4, 20, 12, 20, 0))
+                ),
+                Date(),
+                "abc"
+
+        ))
+        assertThat(user.totalSessionTime).isEqualTo(80)
     }
 }
