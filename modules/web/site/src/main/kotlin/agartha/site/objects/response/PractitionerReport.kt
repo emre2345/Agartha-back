7bb89074a017541393634033a5e8cea2826747f2
RebeccaFransson <rebecca@kollektiva.se>
2018-04-26 12:50:34 +0200

    fixed more

--- modules/web/site/src/main/kotlin/agartha/site/objects/response/PractitionerReport.kt
+++ modules/web/site/src/main/kotlin/agartha/site/objects/response/PractitionerReport.kt
@@ -6,7 +6,7 @@ import agartha.data.objects.SessionDBO
  * Purpose of this file holding information about the current user/practitioner
  *
  * Created by Jorgen Andersson on 2018-04-23.
- * @param userId database user id
+ * @param practitionerId database user id
  * @param lastSessionTime number of minutes for last session
  * @param totalSessionTime total number of minutes for user sessions
  */
