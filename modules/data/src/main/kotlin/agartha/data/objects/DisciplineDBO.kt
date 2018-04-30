3cd402e6d5bf0242678104fedc29120a0363fc35
RebeccaFransson <rebecca@kollektiva.se>
2018-04-30 13:07:20 +0200

    created new discipline object

---
+++ modules/data/src/main/kotlin/agartha/data/objects/DisciplineDBO.kt
@@ -0,0 +1,12 @@
+package agartha.data.objects
+
+/**
+ * Purpose of this file is representing data object for a practice
+ *
+ * Created by Rebecca Fransson on 2018-04-30.
+ */
+data class DisciplineDBO(
+        // Title of Discipline
+        val title: String,
+        // Practices for the discipline
+        val practices : List<PracticeDBO> = emptyList())
