a73b78a9e8b77ddad4f7e60bc3a327434897dd45
Jörgen Andersson <jorgen@kollektiva.se>
2018-05-03 09:27:42 +0200

    Comments

--- modules/web/site/src/main/kotlin/agartha/site/controllers/PractitionerController.kt
+++ modules/web/site/src/main/kotlin/agartha/site/controllers/PractitionerController.kt
@@ -54,9 +54,10 @@ class PractitionerController {
         val user: PractitionerDBO = getPractitionerFromDataSource(userId)
         // Create Report for current user
         val practitionerReport: PractitionerReport = PractitionerReport(user)
-        val startTime: LocalDateTime = LocalDateTime.now().minusMinutes(60)
+        // Created times for getting ongoing sessions
+        val startTime: LocalDateTime = LocalDateTime.now().minusMinutes(15)
         val endTime: LocalDateTime = LocalDateTime.now()
-        // Filter out sessions active during last x minutes and Map to List of Sessions
+        // Filter out sessions active during last x minutes (counted as ongoing) and Map to List of Sessions
         val companionSessions: List<SessionDBO> = SessionUtil.filterSingleSessionActiveBetween(
                 // Get practitioners sessions last 24 hours
                 mService.getPractitionersWithSessionAfter(LocalDateTime.now().minusHours(24)),
