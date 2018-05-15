de9d1a7fb1743cb09e1cedf4cdccc645d9359d3d
RebeccaFransson <rebecca@kollektiva.se>
2018-05-15 09:48:32 +0200

    comments

--- modules/web/site/src/main/kotlin/agartha/site/WebSocketHandler.kt
+++ modules/web/site/src/main/kotlin/agartha/site/WebSocketHandler.kt
@@ -17,13 +17,23 @@ class WebSocketHandler {
 
     val practitioners = HashMap<Session, PractitionerDBO>()
 
+    /**
+     * When a practitioner has connected to the WebSocket
+     */
     @OnWebSocketConnect
     fun connected(session: Session) = println("session connected")
 
+    /**
+     * When the webSocket receives a message this is where is it attended
+     * TODO: Use our own mapper + strings to enums
+     * Message-types:
+     * 'startSession' - adds the practitioner to the map
+     *                  and broadcasts the new practitioner to the others in the session
+     *                  Emit the companions in the map to the new practitioner
+     */
     @OnWebSocketMessage
     fun message(session: Session, message: String) {
         val json = ObjectMapper().readTree(message)
-        // {type: "join/say", data: "name/msg"}
         when (json.get("type").asText()) {
             "startSession" -> {
                 // Get the practitioner
@@ -40,18 +50,33 @@ class WebSocketHandler {
     }
 
 
+    /**
+     * Closing the webSocket results in:
+     * - removing practitioner from map
+     * - Broadcast to everybody else that a companion left
+     */
     @OnWebSocketClose
     fun disconnect(session: Session, code: Int, reason: String?) {
         println("closing")
-        // remove the user from our list
-        val user = practitioners.remove(session)
-        // notify all other users this user has disconnected
-        if (user != null) broadcastToOthers(session, Message("companionLeft", practitioners))
+        // Remove the practitioner from the list
+        val practitioner: PractitionerDBO? = practitioners.remove(session)
+        // Notify all other practitioners this practitioner has left the session
+        if (practitioner != null) broadcastToOthers(session, Message("companionLeft", practitioners))
     }
 
 
+    /**
+     * Send a message to a specific webSocket-session
+     * @param session - the practitioners webSocket-session
+     * @param message - the message for the client
+     */
     fun emit(session: Session, message: Message) = session.remote.sendString(jacksonObjectMapper().writeValueAsString(message))
-    fun broadcast(message: Message) = practitioners.forEach() { emit(it.key, message) }
+
+    /**
+     * Broadcast a message to everybody except a specific session
+     * @param session - the practitioners webSocket-session
+     * @param message - the message for the client
+     */
     fun broadcastToOthers(session: Session, message: Message) = practitioners.filter { it.key != session }.forEach() { emit(it.key, message)}
 
 }
