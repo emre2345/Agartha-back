a0e368f37c0bc7cb2c389e7cafd4fcb31ddfc23e
RebeccaFransson <rebecca@kollektiva.se>
2018-06-07 13:45:10 +0200

    changed the WebSocketMessage

--- modules/web/site/src/main/kotlin/agartha/site/objects/webSocket/WebSocketMessage.kt
+++ modules/web/site/src/main/kotlin/agartha/site/objects/webSocket/WebSocketMessage.kt
@@ -2,5 +2,8 @@ package agartha.site.objects.webSocket
 
 /**
  * Small class to hold a type and a message that is sent from and to the client
+ * @param event - String. A WebSocketEvent (used in both to and from client)
+ * @param data - String. Data that is stringified. (used in both to and from client)
+ * @param practitionersSession - String. The practitioners session (used only when sending connected or closed to client)
  */
-class WebSocketMessage(val event: String, val data: Any)
+class WebSocketMessage(val event: String, val data: String, val practitionersSession: String = "")
