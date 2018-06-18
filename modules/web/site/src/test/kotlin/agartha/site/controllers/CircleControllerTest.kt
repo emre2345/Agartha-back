8b84a7762013a209983ad9f3d75f547b3eee28ef
Jörgen Andersson <jorgen@kollektiva.se>
2018-06-18 11:56:52 +0200

    tests

--- modules/web/site/src/test/kotlin/agartha/site/controllers/CircleControllerTest.kt
+++ modules/web/site/src/test/kotlin/agartha/site/controllers/CircleControllerTest.kt
@@ -1,12 +1,10 @@
 package agartha.site.controllers
 
 import agartha.common.config.Settings.Companion.SPIRIT_BANK_START_POINTS
-import agartha.data.objects.CircleDBO
-import agartha.data.objects.PractitionerDBO
-import agartha.data.objects.SpiritBankLogItemDBO
-import agartha.data.objects.SpiritBankLogItemType
+import agartha.data.objects.*
 import agartha.site.controllers.mocks.MockedPractitionerService
 import agartha.site.controllers.utils.ControllerUtil
+import agartha.site.objects.response.CircleReport
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.BeforeClass
@@ -92,22 +90,37 @@ class CircleControllerTest {
                 sessions = listOf(),
                 circles = listOf(
                         CircleDBO(
-                                name = "",
-                                description = "",
+                                _id = "c1",
+                                name = "CName",
+                                description = "CDesc",
                                 startTime = LocalDateTime.now().minusMinutes(30),
                                 endTime = LocalDateTime.now().plusMinutes(90),
                                 intentions = listOf(),
                                 disciplines = listOf(),
                                 minimumSpiritContribution = 5)),
                 spiritBankLog = listOf(
-                        SpiritBankLogItemDBO(type = SpiritBankLogItemType.START, points = SPIRIT_BANK_START_POINTS)
+                        SpiritBankLogItemDBO(created = LocalDateTime.now().minusHours(4), type = SpiritBankLogItemType.START, points = SPIRIT_BANK_START_POINTS),
+                        SpiritBankLogItemDBO(created = LocalDateTime.now(), type = SpiritBankLogItemType.START, points = 3)
                 )))
 
         // User without enough points in spiritBankLog
         mockedService.insert(PractitionerDBO(
                 _id = "d",
                 created = LocalDateTime.now(),
-                sessions = listOf(),
+                sessions = listOf(
+                        SessionDBO(
+                                discipline = "D",
+                                intention = "I",
+                                circle = CircleDBO(
+                                        _id = "c1",
+                                        name = "CName",
+                                        description = "CDesc",
+                                        intentions = listOf(),
+                                        disciplines = listOf(),
+                                        startTime = LocalDateTime.now().minusMinutes(30),
+                                        endTime = LocalDateTime.now().plusMinutes(30),
+                                        minimumSpiritContribution = 12))
+                ),
                 circles = listOf(),
                 spiritBankLog = listOf(
                         SpiritBankLogItemDBO(type = SpiritBankLogItemType.START, points = SPIRIT_BANK_START_POINTS),
@@ -244,5 +257,65 @@ class CircleControllerTest {
         val response = testController.testServer.execute(request)
         val practitioner = ControllerUtil.stringToObject(String(response.body()), PractitionerDBO::class.java)
         assertThat(practitioner.circles.size).isEqualTo(1)
+    }
+
+    @Test
+    fun receipt_status_200() {
+        setup()
+        val request = testController.testServer.get("/circle/receipt/c/c1", false)
+        val response = testController.testServer.execute(request)
+        assertThat(response.code()).isEqualTo(200)
+    }
+
+    @Test
+    fun receipt_practitionerMissing_400(){
+        setup()
+        val request = testController.testServer.get("/circle/receipt/s/c1", false)
+        val response = testController.testServer.execute(request)
+        assertThat(response.code()).isEqualTo(400)
+    }
+
+    @Test
+    fun receipt_practitionerNotCreator_400(){
+        setup()
+        val request = testController.testServer.get("/circle/receipt/a/c1", false)
+        val response = testController.testServer.execute(request)
+        assertThat(response.code()).isEqualTo(400)
+    }
+
+    @Test
+    fun receipt_circleName_CName() {
+        setup()
+        val request = testController.testServer.get("/circle/receipt/c/c1", false)
+        val response = testController.testServer.execute(request)
+        val report = ControllerUtil.stringToObject(String(response.body()), CircleReport::class.java)
+        assertThat(report.name).isEqualTo("CName")
+    }
+
+    @Test
+    fun receipt_circleDescripition_CDesc() {
+        setup()
+        val request = testController.testServer.get("/circle/receipt/c/c1", false)
+        val response = testController.testServer.execute(request)
+        val report = ControllerUtil.stringToObject(String(response.body()), CircleReport::class.java)
+        assertThat(report.description).isEqualTo("CDesc")
+    }
+
+    @Test
+    fun receipt_circleSessions_3() {
+        setup()
+        val request = testController.testServer.get("/circle/receipt/c/c1", false)
+        val response = testController.testServer.execute(request)
+        val report = ControllerUtil.stringToObject(String(response.body()), CircleReport::class.java)
+        assertThat(report.numberOfPractitioners).isEqualTo(1)
+    }
+
+    @Test
+    fun receipt_circlePoints_1() {
+        setup()
+        val request = testController.testServer.get("/circle/receipt/c/c1", false)
+        val response = testController.testServer.execute(request)
+        val report = ControllerUtil.stringToObject(String(response.body()), CircleReport::class.java)
+        assertThat(report.generatedPoints).isEqualTo(3)
     }
 }
