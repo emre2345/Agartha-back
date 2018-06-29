90bf5ac0b1a3de4b94e044c71f4aff571701b219
Jörgen Andersson <jorgen@kollektiva.se>
2018-06-29 10:39:33 +0200

    Might this work?

---
+++ modules/web/site/src/main/kotlin/agartha/site/objects/response/AudioVars.kt
@@ -0,0 +1,8 @@
+package agartha.site.objects.response
+
+/**
+ * Purpose of this file is ...
+ *
+ * Created by Jorgen Andersson on 2018-06-29.
+ */
+data class AudioVars(val apiKey: String, val sessionId: String, val token: String)
